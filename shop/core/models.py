15b561ad5cc26c238d4d6408a963e47a98115d34
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-05 20:10:30 +0330

    add BaceModel in core

--- shop/core/models.py
+++ shop/core/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class BaseModel(models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        abstract = True
