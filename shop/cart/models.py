1d6af24af3453cdcb49c707a5a5df6a8c45b7e79
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-07 04:42:55 +0330

    add cart models

--- shop/cart/models.py
+++ shop/cart/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from core.models import BaseModel
-
+from django.utils.translation import gettext_lazy as _
+from product.models import Product
 # Create your models here.
 
 User = get_user_model()
@@ -9,5 +10,38 @@ User = get_user_model()
 
 
 class Cart(BaseModel):
+    class Status(models.IntegerChoices):
+        """
+            This is a class for status choices.
+        """
+        ORDERED = 1, _('Ordered')
+        NEW = 2, _('New')
+
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='carts')
-    
+    status = models.IntegerField(choices=Status.choices, default=Status.NEW)
+
+    def __str__(self) -> str:
+        return f'{self.user}:{self.status}'
+
+
+
+class CartItem(BaseModel):
+    class Status(models.IntegerChoices):
+        """
+            This is a class for status choices.
+        """
+        ACTIVE = 1, _('Active')
+        DELETE = 2, _('Delete')
+
+    cart =models.ForeignKey(Cart, on_delete=models.CASCADE,related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT,related_name='cart_items')
+    quantity = models.PositiveIntegerField()
+    status = models.IntegerField(choices=Status.choices, default=Status.ACTIVE)
+
+    def __str__(self) -> str:
+        return str(self.id)
+
+
+
+
