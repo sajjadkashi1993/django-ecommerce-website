c77c7a9ccd455e8c676d53186c6e93e5b29590c4
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-10 00:36:28 +0330

    verbose order models

--- shop/order/models.py
+++ shop/order/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from core.models import BaseModel
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
 from discount.models import Coupon
 from product.models import Product
 from core.validators import PhoneValidator
 
 User = get_user_model()
-
-# Create your models here.
 
 
 class Order(BaseModel):
@@ -36,33 +36,39 @@ class Order(BaseModel):
         """
             This is a class for status choices.
         """
-        New = 1, 'New'
-        Checkout = 2, 'Checkout'
-        Paid = 3, 'Paid'
-        Failed = 4, 'Failed'
-        Shipped = 5, 'Shipped'
-        Delivered = 6, 'Delivered'
-        Returned = 7, 'Returned'
-        Complete = 8, 'Complete'
+        New = 1, _('New')
+        Checkout = 2, _('Checkout')
+        Paid = 3, _('Paid')
+        Failed = 4, _('Failed')
+        Shipped = 5, _('Shipped')
+        Delivered = 6, _( 'Delivered')
+        Returned = 7, _('Returned')
+        Complete = 8, _('Complete')
 
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='orders')
+        User, on_delete=models.CASCADE,verbose_name=_('user'), related_name='orders')
     status = models.IntegerField(choices=STATUS.choices, default=STATUS.New)
     coupon = models.ForeignKey(
-        Coupon, on_delete=models.SET_NULL, related_name='orders', null=True, blank=True)
-    sub_total = models.DecimalField(max_digits=20, decimal_places=2)
-    tax = models.DecimalField(max_digits=20, decimal_places=2)
-    shipping = models.DecimalField(max_digits=20, decimal_places=2)
-    total = models.DecimalField(max_digits=20, decimal_places=2)
-    discount = models.DecimalField(max_digits=20, decimal_places=2)
-    grand = models.DecimalField(max_digits=20, decimal_places=2)
-    receiver_name = models.CharField(max_length=100)
-    receiver_mobile = models.CharField(max_length=15, validators=[PhoneValidator()])
-    country = models.CharField(max_length=100, default='iran')
-    province = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    content = models.TextField(null=True, blank=True)
+        Coupon, on_delete=models.SET_NULL,verbose_name=_('coupon'), related_name='orders', null=True, blank=True)
+    sub_total = models.DecimalField(_('sub total'), max_digits=20, decimal_places=2)
+    tax = models.DecimalField(_('tax'), max_digits=20, decimal_places=2)
+    shipping = models.DecimalField(_('shipping'), max_digits=20, decimal_places=2)
+    total = models.DecimalField(_('total'), max_digits=20, decimal_places=2)
+    discount = models.DecimalField(_('discount'), max_digits=20, decimal_places=2)
+    grand = models.DecimalField(_('grand'), max_digits=20, decimal_places=2)
+    receiver_name = models.CharField(_('receiver name'), max_length=100)
+    receiver_mobile = models.CharField(_('receiver mobile'), max_length=15, validators=[PhoneValidator()])
+    country = models.CharField(_('country'), max_length=100, default='iran')
+    province = models.CharField(_('province'), max_length=100)
+    city = models.CharField(_('city'), max_length=100)
+    address = models.CharField(_('address'), max_length=100)
+    content = models.TextField(_('content'), null=True, blank=True)
+
+
+    class Meta:
+        verbose_name = _("order")
+        verbose_name_plural = _('orders')
+
 
     def __str__(self) -> str:
         return f'{self.user}, {self.id}'
@@ -81,13 +87,19 @@ class OrderItem(BaseModel):
         Updated At:	It stores the date and time at which the ordered item is updated.
     """
     product = models.ForeignKey(
-        Product, on_delete=models.PROTECT, related_name='order_items')
+        Product, on_delete=models.PROTECT,verbose_name=_('Product'), related_name='order_items')
     order = models.ForeignKey(
-        Order, on_delete=models.CASCADE, related_name='items')
-    warehouse_code = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=20, decimal_places=2)
-    discount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField()
+        Order, on_delete=models.CASCADE,verbose_name=_('Order'), related_name='items')
+    warehouse_code = models.CharField(_('warehouse code'), max_length=50)
+    price = models.DecimalField(_('price'), max_digits=20, decimal_places=2)
+    discount = models.DecimalField(_('discount'), max_digits=20, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(_('quantity'), )
+
+
+    class Meta:
+        verbose_name = _("Order Item")
+        verbose_name_plural = _('Order Items')
+
 
     def __str__(self) -> str:
         return f'{self.product}:{self.quantity}'
@@ -105,21 +117,26 @@ class Transaction(BaseModel):
         Content:	The column used to store the additional details of the transaction.
     """
     class STATUS(models.IntegerChoices):
-        New = 1, 'New'
-        Cancelled = 2, 'Cancelled'
-        Failed = 3, 'Failed'
-        Pending = 4, 'Pending'
-        Declined = 5, 'Declined'
-        Rejected = 6, 'Rejected'
-        Success = 7, 'Success'
+        New = 1, _('New')
+        Cancelled = 2, _('Cancelled')
+        Failed = 3, _('Failed')
+        Pending = 4, _('Pending')
+        Declined = 5, _('Declined')
+        Rejected = 6, _('Rejected')
+        Success = 7, _('Success')
 
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='transactions')
+        User, on_delete=models.CASCADE, verbose_name=_('user'), related_name='transactions')
     order = models.ForeignKey(
-        Order, on_delete=models.CASCADE, related_name='transactions')
-    code = models.CharField(max_length=50)
-    status = models.IntegerField(choices=STATUS.choices, default=STATUS.New)
-    content = models.TextField(null=True, blank=True)
+        Order, on_delete=models.CASCADE,verbose_name=_('order'), related_name='transactions')
+    code = models.CharField(_('code'), max_length=50)
+    status = models.IntegerField(_('status'), choices=STATUS.choices, default=STATUS.New)
+    content = models.TextField(_('content'), null=True, blank=True)
+
+
+    class Meta:
+        verbose_name = _("Transaction")
+        verbose_name_plural = _('Transactions')
 
     def __str__(self) -> str:
         return f'{self.order}:{self.code}'
