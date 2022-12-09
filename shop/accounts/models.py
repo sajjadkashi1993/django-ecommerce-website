5aef20a0c78761f71533575fdb147dd399c1805d
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-09 17:56:52 +0330

    add  verbose_name

--- shop/accounts/models.py
+++ shop/accounts/models.py
@@ -1,41 +1,46 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from core.models import BaseModel
-
+from django.utils.translation import gettext_lazy as _
 from core.validators import PhoneValidator
 
 from .managers import UserManager
 
 
 class User(AbstractUser):
-    ROLE_CHOICES = (('c', 'Customer'),
-                    ('e1', 'Employee1'),
-                    ('e2', 'Employee2'),
-                    )
+    class ROLE(models.IntegerChoices):
+        USER = 0, _('User')
+        CUSTOMER = 1, _('Customer')
+        ADMIM = 2, _('Admin')
+        EMPLOYEE = 3, _('Employee')
 
     username = None
-
     phone_validator = PhoneValidator()
 
     phone = models.CharField(
-        "phone number",
+        _("phone number"),
         max_length=11,
         unique=True,
         help_text=(
-            "Required. 11 character. digits only."
+            _("Required. 11 character. digits only.")
         ),
         validators=[phone_validator],
         error_messages={
-            "unique": ("A user with that phone already exists."),
+            "unique": _("A user with that phone already exists."),
         },
     )
 
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='c')
+    role = models.CharField(_('role'), max_length=20,
+                            choices=ROLE.choices, default=ROLE.USER)
 
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
 
     def __str__(self):
         return self.phone
@@ -43,16 +48,22 @@ class User(AbstractUser):
 
 class ProfileUser(BaseModel):
     class GENDER(models.IntegerChoices):
-        MALE = 1, 'Male'
-        FEMALE = 2, 'Female'
-        OTHER = 3, 'Other'
+        MALE = 1, _('Male')
+        FEMALE = 2, _('Female')
+        OTHER = 3, _('Other')
 
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='profile')
-    gender = models.IntegerField(choices=GENDER.choices, null=True, blank=True)
-    birthday = models.DateField(null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to=f'profile/', null=True, blank=True)
+        User, on_delete=models.CASCADE, related_name='profile', verbose_name=_('user'))
+    gender = models.IntegerField(
+        _('gender'), choices=GENDER.choices, null=True, blank=True)
+    birthday = models.DateField(_('birthday'), null=True, blank=True)
+    bio = models.TextField(_('bio'), null=True, blank=True)
+    image = models.ImageField(
+        _('image'), upload_to=f'profile/', null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("profile")
+        verbose_name_plural = _("profiles")
 
     def __str__(self) -> str:
         return f'profile for {self.user}'
@@ -60,15 +71,16 @@ class ProfileUser(BaseModel):
 
 class Address(BaseModel):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='addresses')
-    country = models.CharField(max_length=100, default='iran')
-    province = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    adderess = models.TextField(max_length=100)
-    postal_code = models.CharField(max_length=20)
+        User, on_delete=models.CASCADE, related_name='addresses',verbose_name=_("user"))
+    country = models.CharField(_("country"), max_length=100, default='iran')
+    province = models.CharField(_("province"), max_length=100)
+    city = models.CharField(_("city"), max_length=100)
+    adderess = models.TextField(_("adderess"), max_length=100)
+    postal_code = models.CharField(_("postal code"), max_length=20)
 
     class Meta:
-        verbose_name_plural = 'Adresses'
+        verbose_name = _("address")
+        verbose_name_plural = _('adresses')
 
     def __str__(self):
         return f'{self.country}, {self.province}, {self.city}, {self.adderess}'
@@ -76,20 +88,24 @@ class Address(BaseModel):
 
 class OtpCode(BaseModel):
     phone = models.CharField(
-        "phone number",
+        _("phone number"),
         max_length=11,
         unique=True,
         help_text=(
-            "Required. 11 character. digits only."
+            _("Required. 11 character. digits only.")
         ),
         validators=[PhoneValidator()],
         error_messages={
-            "unique": ("A user with that phone already exists."),
+            "unique": _("A user with that phone already exists."),
         },
     )
 
-    code = models.CharField(max_length=8)
-    number_try = models.PositiveSmallIntegerField(default=0)
+    code = models.CharField(_('code'), max_length=8)
+    number_try = models.PositiveSmallIntegerField(_('number try'), default=0)
+
+    class Meta:
+        verbose_name = _("OTP Code")
+        verbose_name_plural = _('OTP Codes')
 
     def __str__(self):
         return self.code
