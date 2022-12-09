ac1928b1972b0f4f911637c6d481c27fbc9e9b82
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-10 00:36:03 +0330

    verbose discount models

--- shop/discount/models.py
+++ shop/discount/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from core.models import BaseModel
-
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
 
 
 class Coupon(BaseModel):
-    code = models.CharField(max_length=10)
-    min_purchase = models.DecimalField(max_digits=20, decimal_places=2)
-    max_discount = models.DecimalField(max_digits=20, decimal_places=2)
-    percent = models.PositiveSmallIntegerField()
-    expire_time = models.DateTimeField()
-    limit_number = models.IntegerField()
-    start_time = models.DateTimeField()
-    event = models.CharField(max_length=100)
+    code = models.CharField(_('code'), max_length=10)
+    min_purchase = models.DecimalField(
+        _('min purchase'), max_digits=20, decimal_places=2)
+    max_discount = models.DecimalField(
+        _('max discount'), max_digits=20, decimal_places=2)
+    percent = models.PositiveSmallIntegerField(_('percent'))
+    expire_time = models.DateTimeField(_('expire time'))
+    limit_number = models.IntegerField(_('limit number'))
+    start_time = models.DateTimeField(_('start time'))
+    event = models.CharField(_('event'), max_length=100)
+
+    class Meta:
+        verbose_name = _("Coupon")
+        verbose_name_plural = _('Coupons')
 
     def __str__(self) -> str:
         return self.code
@@ -23,14 +28,19 @@ class Discount(BaseModel):
         percent = 1, 'Percent'
         amount = 2, 'Amount'
 
-    type = models.IntegerField(choices=TYPE.choices)
-    percent = models.PositiveSmallIntegerField(null=True, blank=True)
-    amount = models.DecimalField(
-        max_digits=20, decimal_places=2, null=True, blank=True)
+    type = models.IntegerField(_('type'), choices=TYPE.choices)
+    percent = models.PositiveSmallIntegerField(
+        _('percent'), null=True, blank=True)
+    amount = models.DecimalField(_('amount'),
+                                 max_digits=20, decimal_places=2, null=True, blank=True)
 
-    start_time = models.DateTimeField()
-    expire_time = models.DateTimeField()
-    event = models.CharField(max_length=100)
+    start_time = models.DateTimeField(_('start time'), )
+    expire_time = models.DateTimeField(_('expire time'), )
+    event = models.CharField(_('event'), max_length=100)
+
+    class Meta:
+        verbose_name = _("Discount")
+        verbose_name_plural = _('Discounts')
 
     def __str__(self) -> str:
         return str(self.percent)
