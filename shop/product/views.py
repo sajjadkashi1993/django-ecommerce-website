7237fb18f9879df800bc62b6c86604a03178b38c
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-10 23:38:39 +0330

    format doc

--- shop/product/views.py
+++ shop/product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView, BaseListView
+from .models import Product
 # Create your views here.
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product/product_list.html'
