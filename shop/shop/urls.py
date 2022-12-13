810edf7296bd2809cf4ea36cdc854eaadab59514
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-13 13:37:07 +0330

    update urls

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('', include('home.urls', namespace='home')),
     path("accounts/", include("django.contrib.auth.urls")),
     path("product/", include("product.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:  # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
