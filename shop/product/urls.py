04f05d825b63c9896924f132d86a094b13d537f0
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-13 12:11:36 +0330

    add url product

--- shop/product/urls.py
+++ shop/product/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from django.views.generic import TemplateView
-from .views import ProductListView
+from .views import ProductListView, ProductDetailtView
 
 app_name='product'
 urlpatterns = [
     path('shop/', ProductListView.as_view(), name='shop'),
     path('shop/<str:slug>', ProductListView.as_view(), name='cat'),
-    path('product-sale/', TemplateView.as_view(template_name='product/product-sale.html'), name='product-sale'),
+    path('<int:pk>/', ProductDetailtView.as_view(), name='product'),
 ]
