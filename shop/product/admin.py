59de4235da8880b89d066a8fe8264247610af1d2
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-13 11:39:34 +0330

    update admin image

--- shop/product/admin.py
+++ shop/product/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from .models import Category, Product, Gallery, Image, Price, Property
 # Register your models here.
 
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ('gallery', 'image_alt', 'name')
-    raw_id_fields = ('gallery', )
+
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display= ('title', 'slug', 'is_navbar', 'parent')
@@ -13,8 +12,9 @@ class CategoryAdmin(admin.ModelAdmin):
     list_editable = ('is_navbar','parent')
     raw_id_fields = ('parent',)
 
-class GalleryInline(admin.TabularInline):
+class GalleryInline(admin.StackedInline):
     model = Gallery
+
 
 
 class PriceInline(admin.TabularInline):
@@ -34,3 +34,7 @@ class CustomUserAdmin(admin.ModelAdmin):
     raw_id_fields =('discount',)
 
 
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ( 'gallery','image_alt', 'name')
+    raw_id_fields = ('gallery', )
