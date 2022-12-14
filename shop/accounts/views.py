2f2f4dc6f6e0c318ddf9b11313cfa11407af8a26
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-15 00:27:08 +0330

    update views

--- shop/accounts/views.py
+++ shop/accounts/views.py
@@ -1,9 +1,12 @@
+from datetime import timedelta
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.contrib import messages
 from .forms import UserLoginRegisterForm, VerifyCodeForm
 from django.contrib.auth import get_user_model, login, logout
+from django.utils.translation import gettext_lazy as _
+from django.utils.timezone import now
 
 import random
 from .models import OtpCode
@@ -60,7 +63,7 @@ class VerifyCodeview(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             code = form.cleaned_data.get('code')
-            if str(code) == otp.code:
+            if str(code) == otp.code and otp.created_at - now() <= timedelta(minutes=2):
                 if User.objects.filter(phone=phone).exists():
                     user = User.objects.get(phone=phone)
                 else:
@@ -68,16 +71,17 @@ class VerifyCodeview(View):
 
                 login(request, user)
                 otp.delete()
-                messages.success(request, 'log in sucessfully', 'success')
+                messages.success(request, _('log in sucessfully'), 'success')
                 return redirect('home:home')
             else:
-                messages.error(request, 'this code is wrong', 'danger')
+                messages.error(request, _('this code is wrong or time out'), 'danger')
                 return redirect('accounts:verify')
-        return render(request, 'accounts/verify.html', {'form': form})
+        return render(request, _('accounts/verify.html'), {'form': form})
 
 
 class LogoutView(View):
     def get(self, request):
         if request.user.is_authenticated:
+            messages.success(request, _('log out sucessfully'), 'success')
             logout(request)
         return redirect('home:home')
