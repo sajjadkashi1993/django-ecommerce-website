84308e3951685a7b52abe07d8795e8034a2d55e2
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-16 01:26:22 +0330

    update send_otp_code

--- shop/accounts/utils.py
+++ shop/accounts/utils.py
@@ -1,3 +1,25 @@
+from core.sms import SmsAPI, APIException, HTTPException
+from django.conf import settings
 
-def send_otp_code(phone,otp_code):
-    pass
+
+def send_otp_code(phone: str, otp_code: str):
+    try:
+        api = SmsAPI(settings.SMS_API_KEY)
+        params = {
+            "mobile": phone,
+            "templateId": 100000,
+            "parameters": [
+                {
+                    "name": "code",
+                    "value": otp_code
+                }
+
+            ]
+        }
+        response = api.sms_verify_send(params)
+        print(12222222222222222222222222222)
+        print(str(response))
+    except APIException as e:
+        print(str(e))
+    except HTTPException as e:
+        print(str(e))
