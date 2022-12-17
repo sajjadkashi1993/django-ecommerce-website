e268e9d4cc7551b29a72b2672d4ede6dad0dd236
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-17 19:16:18 +0330

    change classview contact

--- shop/home/urls.py
+++ shop/home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import ContactUsView
 
 
 app_name='home'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('about-us/', TemplateView.as_view(template_name='about-us.html'), name='about-us'),
-    path('contact-us/', TemplateView.as_view(template_name='contact-us.html'), name='contact-us'),
+    path('contact-us/', ContactUsView.as_view(), name='contact-us'),
 ]
