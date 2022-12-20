879ee183c111c6bd00f5a8aaf5d791d1dba96f2e
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-20 12:38:03 +0330

    update admin comment

--- shop/comment/admin.py
+++ shop/comment/admin.py
@@ -6,9 +6,9 @@ from .models import Comment
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'nickname','product', 'body', 'created_at', 'status')
+    list_display = ('user', 'nickname','product', 'created_at', 'status')
     list_editable = ('status',)
     list_filter = ('status', 'product')
     date_hierarchy = 'created_at'
-    ordering = ('created_at',)
+    ordering = ('-created_at',)
     raw_id_fields = ('parent',)
