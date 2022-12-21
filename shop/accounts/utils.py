8d6f2b662a3170bbc49df6ebd865e1bec88a3ba8
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-21 18:10:40 +0330

    update utils

--- shop/accounts/utils.py
+++ shop/accounts/utils.py
@@ -1,10 +1,10 @@
 from core.sms import SmsAPI, APIException, HTTPException
-from django.conf import settings
+from API_keys import SMS_API_KEY
 
 
 def send_otp_code(phone: str, otp_code: str):
     try:
-        api = SmsAPI(settings.SMS_API_KEY)
+        api = SmsAPI(SMS_API_KEY)
         params = {
             "mobile": phone,
             "templateId": 100000,
@@ -17,7 +17,6 @@ def send_otp_code(phone: str, otp_code: str):
             ]
         }
         response = api.sms_verify_send(params)
-        print(12222222222222222222222222222)
         print(str(response))
     except APIException as e:
         print(str(e))
