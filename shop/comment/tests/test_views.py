831d7edc045fce50db2bf748991479ded892c68f
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-22 01:12:39 +0330

    comment view test

--- shop/comment/tests/test_views.py
+++ shop/comment/tests/test_views.py
@@ -1,1 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from model_bakery import baker
+from ..forms import CommentForm
+
+class TestCreateCommentView(TestCase):
+
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_create_comment_post_valid(self):
+        response = self.client.post(reverse('comment:create_comment', kwargs={'pk':1}))
+        self.assertEqual(response.status_code, 200)
