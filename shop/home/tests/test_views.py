e13107358e2fbce90c50028d25da91b86c18a144
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-21 23:49:37 +0330

    test view home

--- shop/home/tests/test_views.py
+++ shop/home/tests/test_views.py
@@ -1,1 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+ 
+class TestContactUsView(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_contact_get(self):
+        response = self.client.get(reverse('home:contact-us'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact-us.html')
+
+    def test_contact_post_valid(self):
+        response = self.client.post(reverse('home:contact-us'))
+        self.assertEqual(response.status_code, 200)
+
+
+
+
--- shop/home/views.py
+++ shop/home/views.py
@@ -3,12 +3,11 @@ from django.views.generic import View
 from django.http import JsonResponse
 
 
-# Create your views here.
 class ContactUsView(View):
-    def get(self,request):
-        return render(request,'contact-us.html')
+    def get(self, request):
+        return render(request, 'contact-us.html')
 
     def post(self, requst):
-        print(33333333333333333, requst.POST['name'])
-        #TODO mail contact us 
-        return JsonResponse({'message':'Your message has been successfully sent', 'status':'success'})
+        print(33333333333333333, requst.POST)
+        # TODO mail contact us
+        return JsonResponse({'message': 'Your message has been successfully sent', 'status': 'success'})
