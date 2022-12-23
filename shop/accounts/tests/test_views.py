b8c2afb13ec025465de386be7dddd63187e7853b
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-23 09:46:06 +0330

    update test views

--- shop/accounts/tests/test_views.py
+++ shop/accounts/tests/test_views.py
@@ -4,6 +4,7 @@ from model_bakery import baker
 from ..forms import UserLoginRegisterForm
 from ..models import OtpCode
 
+
 class TestLoginRegisteruser(TestCase):
 
     def setUp(self) -> None:
@@ -13,12 +14,13 @@ class TestLoginRegisteruser(TestCase):
         response = self.client.get(reverse('accounts:login'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'accounts/login.html')
-        self.failUnless(response.context['form'],UserLoginRegisterForm)
+        self.failUnless(response.context['form'], UserLoginRegisterForm)
 
     def test_login_register_post_valid(self):
-        response = self.client.post(reverse('accounts:login'),data={'phone':'09128812994'})
+        response = self.client.post(reverse('accounts:login'), data={
+                                    'phone': '09128812994'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('accounts:verify'))
-        self.assertEqual(OtpCode.objects.count(),1)
+        self.assertEqual(OtpCode.objects.count(), 1)
         # self.assertEqual(response.session['user_login_info'],'1')
         # self.mes()
