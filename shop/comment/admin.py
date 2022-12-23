02c288c1c9bea547a948368191047a1aaa51693d
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-23 14:49:36 +0330

    update actione comment

--- shop/comment/admin.py
+++ shop/comment/admin.py
@@ -5,24 +5,26 @@ from django.contrib import messages
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    actions = ['delete_selected', 'make_approved', 'make_rejected']
     list_display = ('user', 'nickname','product', 'body', 'status')
     list_editable = ('status',)
     list_filter = ('status', 'product')
     date_hierarchy = 'created_at'
     ordering = ('-created_at',)
     raw_id_fields = ('parent',)
+    empty_value_display = '-empty-'
+    search_fields = ('comment','product')
 
+    @admin.action(description='Make Approved')
     def make_approved(modeladmin, request, queryset):
         queryset.update(status = 2)
         messages.success(request, "Selected Record(s) Marked as Approved Successfully !!")
-
+    
+    @admin.action(description='Make Rejected')
     def make_rejected(modeladmin, request, queryset):
         queryset.update(status = 3)
         messages.success(request, "Selected Record(s) Marked as Rejected Successfully !!")
 
-
-    admin.site.add_action(make_approved, "Make Approved")
-    admin.site.add_action(make_rejected, "Make Rejected")
 
 
 
