a45f315729dc89381f1397bd0b74a22925ddb06e
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-27 00:57:49 +0330

    add category viewset

--- shop/product/api/urls.py
+++ shop/product/api/urls.py
@@ -1,7 +1,17 @@
 from django.urls import include, path
-from .views import productApiViews
+from .views import ProductViewSet, CategorytViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('products',ProductViewSet )
+router.register('categories',CategorytViewSet )
 app_name='product'
+
+
+# urlpatterns = router.urls
+# 
+
+
 urlpatterns = [
-    path('shop/', productApiViews.as_view(), name='products'),
+    path('', include((router.urls, 'product'), namespace='products')),
 ]
--- shop/product/api/views.py
+++ shop/product/api/views.py
@@ -1,12 +1,20 @@
 from rest_framework import views
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from .serializers import ProductSerializer,CategorySerializer
+from ..models import Product, Category
 
 
-class productApiViews(views.APIView):
+class ProductViewSet(ModelViewSet): 
+    queryset = Product.undeleted_objects.all()
+    serializer_class = ProductSerializer
 
-    
-    def get(self, request, format=None):
-        """
-        Return a list of all users.
-        """
-        return Response({'usernames':'sajjad'})
+
+class CategorytViewSet(ModelViewSet): 
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def list(self, request):
+        queryset = Category.objects.filter(parent__isnull=True)
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
