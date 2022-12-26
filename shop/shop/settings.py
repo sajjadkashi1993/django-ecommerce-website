b401d867e882feb5bdb8b524a83f15cb1e90b7d7
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-27 00:56:00 +0330

    comment volt app

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'volt',
+    # 'volt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
