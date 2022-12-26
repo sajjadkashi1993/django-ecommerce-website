43892203916bf3214f87ba0cb598d0b2bb34ef45
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-27 00:56:19 +0330

    comment tags

--- shop/product/templatetags/category_tags.py
+++ shop/product/templatetags/category_tags.py
@@ -1,9 +1,9 @@
-from django import template
-from product.models import Category
+# from django import template
+# from product.models import Category
 
-register = template.Library()
+# register = template.Library()
 
-@register.inclusion_tag('inc/header.html')
-def caregory_navbar():
-        categories = Category.objects.filter(is_navbar=True)
-        return {'categories':categories}
+# @register.inclusion_tag('inc/header.html')
+# def caregory_navbar():
+#         categories = Category.objects.filter(is_navbar=True)
+#         return {'categories':categories}
