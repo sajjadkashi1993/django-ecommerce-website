08323c87aa94da1b01f1ade2d156eeefa125a3cb
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-27 11:22:10 +0330

    add full-detail product url

--- shop/product/api/urls.py
+++ shop/product/api/urls.py
@@ -1,17 +1,14 @@
 from django.urls import include, path
-from .views import ProductViewSet, CategorytViewSet
+from .views import CategorytViewSet, ProductListFullView, ProductRetrievFullView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('products',ProductViewSet )
-router.register('categories',CategorytViewSet )
-app_name='product'
-
-
-# urlpatterns = router.urls
-# 
+router.register('categories', CategorytViewSet)
+app_name = 'product'
 
 
 urlpatterns = [
     path('', include((router.urls, 'product'), namespace='products')),
+    path('full-detail/', ProductListFullView.as_view() , name='full-detail-list'),
+    path('full-detail/<int:pk>/', ProductRetrievFullView.as_view() , name='full-detail'),
 ]
