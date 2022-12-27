90c4cc0438cbaf6a9dd41ca4567d0b979487aea3
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-27 11:22:47 +0330

    add ProductListFullView

--- shop/product/api/views.py
+++ shop/product/api/views.py
@@ -1,14 +1,12 @@
-from rest_framework import views
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ProductSerializer,CategorySerializer
 from ..models import Product, Category
-
-
-class ProductViewSet(ModelViewSet): 
-    queryset = Product.undeleted_objects.all()
-    serializer_class = ProductSerializer
-
+from .filters import ProductFilter
 
 class CategorytViewSet(ModelViewSet): 
     queryset = Category.objects.all()
@@ -18,3 +16,16 @@ class CategorytViewSet(ModelViewSet):
         queryset = Category.objects.filter(parent__isnull=True)
         serializer = CategorySerializer(queryset, many=True)
         return Response(serializer.data)
+
+class ProductListFullView(generics.ListAPIView): 
+    queryset = Product.undeleted_objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filterset_class = ProductFilter
+    search_fields = ['title', 'warehouse_code', 'content', 'brand']
+    ordering_fields = ['title']
+    pagination_class = PageNumberPagination
+
+class ProductRetrievFullView(generics.RetrieveAPIView): 
+    queryset = Product.undeleted_objects.all()
+    serializer_class = ProductSerializer
