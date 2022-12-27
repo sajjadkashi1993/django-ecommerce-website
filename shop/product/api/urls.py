2c76dda022b62f80eb1635feedfa580037d1a829
sajjadk <sajjad.kashi2012@gmail.com>
2022-12-27 16:22:39 +0330

    add nestet router proprty

--- shop/product/api/urls.py
+++ shop/product/api/urls.py
@@ -1,14 +1,18 @@
 from django.urls import include, path
-from .views import CategorytViewSet, ProductListFullView, ProductRetrievFullView
+from .views import PropertyViewSet, ProductViewSet, CategorytViewSet, ProductListFullView, ProductRetrievFullView
 from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
+from rest_framework_nested import routers
+router = routers.DefaultRouter()
 router.register('categories', CategorytViewSet)
+router.register('products', ProductViewSet)
+
+product_routers = routers.NestedDefaultRouter(router,'products',lookup='product' )
+product_routers.register('proprty',PropertyViewSet, basename='product-proprty')
+
 app_name = 'product'
-
-
 urlpatterns = [
     path('', include((router.urls, 'product'), namespace='products')),
+    path('', include((product_routers.urls))),
     path('full-detail/', ProductListFullView.as_view() , name='full-detail-list'),
     path('full-detail/<int:pk>/', ProductRetrievFullView.as_view() , name='full-detail'),
 ]
