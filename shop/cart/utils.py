3c3e93c5310a3ac50446c43503260d2e4cc457df
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-01 11:04:13 +0330

    add: add_session_cart

--- shop/cart/utils.py
+++ shop/cart/utils.py
@@ -1,9 +1,10 @@
 from product.models import Product
 from product.api.serializers import ProductSimpleSerializer
 from decimal import Decimal
+from .models import CartItem
+from django.db import IntegrityError
 
-
-def session_cart(data: dict):
+def session_cart(data: dict)-> dict:
     cart_item = []
     grand_total = 0
     for k, v in data.items():
@@ -15,3 +16,16 @@ def session_cart(data: dict):
         cart_item.append({'product': serilized_product,
                          'quantity': v, 'sub_total': sub_total})
     return {'cart_items': cart_item, 'grand_total': grand_total}
+
+
+def add_session_cart(user, cart_session:dict):
+    cart = user.carts.get_or_create(user=user, status=2)[0]
+    for k,v in cart_session.items():
+        product = Product.undeleted_objects.get(id=k)
+        try:
+            cart_item = CartItem.objects.create(
+                cart=cart, product=product, quantity=v)
+        except IntegrityError:
+            cart_item = CartItem.objects.get(cart=cart, product=product)
+            cart_item.quantity = v
+            cart_item.save()
