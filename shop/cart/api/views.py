55f988f80af23e4857ab30a9a61594b895c8eb53
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-01 07:12:55 +0330

    add is not authenticated

--- shop/cart/api/views.py
+++ shop/cart/api/views.py
@@ -1,60 +1,61 @@
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.db import IntegrityError
-from ..models import CartItem, Cart
+from ..models import CartItem
 from product.models import Product
 from .serializers import CartSerilizer
+from ..utils import session_cart
+
 
 class AddToCart(APIView):
     def post(self, request):
         if request.user.is_authenticated:
             user = request.user
-            cart = user.carts.get_or_create(user=user,status=2)[0]
+            cart = user.carts.get_or_create(user=user, status=2)[0]
             product = Product.undeleted_objects.get(id=request.POST['product'])
             try:
-                cart_item =CartItem.objects.create(cart=cart,product=product,quantity=request.POST['quantity'])
+                cart_item = CartItem.objects.create(
+                    cart=cart, product=product, quantity=request.POST['quantity'])
             except IntegrityError:
-                cart_item =CartItem.objects.get(cart=cart,product=product)
-                cart_item.quantity=request.POST['quantity']
+                cart_item = CartItem.objects.get(cart=cart, product=product)
+                cart_item.quantity = request.POST['quantity']
                 cart_item.save()
             return Response(CartSerilizer(cart).data, status=status.HTTP_201_CREATED)
         else:
-            # request.session.clear()
             try:
-                request.session['cart'][request.POST['product']]=request.POST['quantity']
+                request.session['cart'][request.POST['product']
+                                        ] = request.POST['quantity']
                 request.session.modified = True
             except KeyError:
-                request.session['cart'] = {request.POST['product']:request.POST['quantity']}
-            return Response( request.session['cart'], status=status.HTTP_201_CREATED)
+                request.session['cart'] = {
+                    request.POST['product']: request.POST['quantity']}
+            return Response(session_cart(request.session['cart']), status=status.HTTP_201_CREATED)
 
 
 class DelCart(APIView):
     def delete(self, request):
         if request.user.is_authenticated:
             user = request.user
-            cart = user.carts.get_or_create(user=user,status=2)[0]
-            product = Product.undeleted_objects.get(id=request.POST['product'])
-            cart_item =CartItem.objects.get(cart=cart,product=product)
+            cart = user.carts.get_or_create(user=user, status=2)[0]
+            product = Product.undeleted_objects.get(id=request.data['product'])
+            cart_item = CartItem.objects.get(cart=cart, product=product)
             cart_item.delete()
-            return Response(CartSerilizer(cart).data, status=status.HTTP_204_NO_CONTENT)
+            return Response(CartSerilizer(cart).data, status=200)
         else:
-            # request.session.clear()
             try:
-                request.session['cart'][request.POST['product']]=request.POST['quantity']
-                request.session.modified = True
+                request.session['cart'].pop(request.data['product'])
             except KeyError:
-                request.session['cart'] = {request.POST['product']:request.POST['quantity']}
-            return Response( request.session['cart'], status=status.HTTP_201_CREATED)
+                pass
+            return Response(session_cart(request.session['cart']), status=status.HTTP_200_OK)
 
 
 class ShowCart(APIView):
     def get(self, request):
         if request.user.is_authenticated:
             user = request.user
-            cart = user.carts.get_or_create(user=user,status=2)[0]
+            cart = user.carts.get_or_create(user=user, status=2)[0]
             serilizer = CartSerilizer(cart)
             return Response(serilizer.data, status=status.HTTP_200_OK)
 
-
+        return Response(session_cart(request.session['cart']), status=status.HTTP_201_CREATED)
