011db8a5aeaec10f29c165cf4dc75153e46db43e
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-02 12:47:55 +0330

    add ChackOutView

--- shop/order/views.py
+++ shop/order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+class ChackOutView(LoginRequiredMixin, View):
+    template_name='order/checkout.html'
+    def get(self,request):
+        return render(request, self.template_name)
