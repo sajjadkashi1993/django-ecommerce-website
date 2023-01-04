1677594bef47a2f088861e58327a39dabf59b804
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-04 22:45:00 +0330

    add order-pay url

--- shop/order/api/urls.py
+++ shop/order/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CheckOutAPIView
+from .views import CheckOutAPIView, OrderPayView
 
 app_name='order'
 urlpatterns = [
     path('checkout/', CheckOutAPIView.as_view(), name='checkout'),
+    path('order-pay/<int:order_id>/', OrderPayView.as_view(), name='order-pay'),
 ]
