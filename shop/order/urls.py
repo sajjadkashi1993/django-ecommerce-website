ec8b7a7d1c7558d7478ae50e51514f2d740d7c4c
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-05 21:31:33 +0330

    add order-verify & order-pay url

--- shop/order/urls.py
+++ shop/order/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ChackOutView
-
+from .views import ChackOutView, OrderVerifyView
+from django.views.generic import TemplateView
 app_name = 'order'
 urlpatterns = [
     path('checkout/', ChackOutView.as_view(), name='checkout'),
+    path('order-verify/', OrderVerifyView.as_view(), name='order-verify'),
+    path('order-pay/', TemplateView.as_view(template_name='order/pay.html'), name='order-pay'),
 ]
