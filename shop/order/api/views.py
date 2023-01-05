1651934e27962350dafa185c89847ab7ea524a56
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-05 17:08:20 +0330

    add transaction

--- shop/order/api/views.py
+++ shop/order/api/views.py
@@ -1,6 +1,5 @@
 from decimal import Decimal
 import json
-from django.shortcuts import redirect
 import requests
 from rest_framework import permissions, status
 from rest_framework.views import APIView
@@ -8,10 +7,12 @@ from rest_framework.response import Response
 from cart.api.serializers import CartSerilizer
 from discount.models import Coupon
 from ..utils import check_discount
-from accounts.models import Address
 from accounts.api.serializers import AddressSerilizers
-from ..models import Order, OrderItem
-from .serializers import OrderItemSerilizers, OrderSerilizers
+from ..models import Order
+from .serializers import OrderSerilizers
+from ..helper import OrderHelper
+from django.db import transaction
+
 class CheckOutAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
@@ -39,13 +40,13 @@ class CheckOutAPIView(APIView):
     def post(self, request, *args, **kwargs):
         user = request.user
         cart = user.carts.get_or_create(user=user, status=2)[0]
-        data ={
-            'user':user.id,
-            'country':request.POST.get('country'), 
-            'province':request.POST.get('province'), 
-            'city':request.POST.get('city'), 
-            'address':request.POST.get('address'), 
-            'postal_code':request.POST.get('postal_code')
+        data = {
+            'user': user.id,
+            'country': request.POST.get('country'),
+            'province': request.POST.get('province'),
+            'city': request.POST.get('city'),
+            'address': request.POST.get('address'),
+            'postal_code': request.POST.get('postal_code')
         }
         address = AddressSerilizers(data=data)
         # if address.is_valid():
@@ -67,45 +68,34 @@ class CheckOutAPIView(APIView):
         shipping = 10
         sub_total = serialized_card.data.get('grand_total')
         total = sub_total + tax + Decimal(shipping)
-        grand = total- Decimal(discount)
+        grand = total - Decimal(discount)
 
         data.update({
-            'coupon':coupon,
-            'sub_total':sub_total ,
-            'tax':tax ,
-            'shipping':shipping ,
-            'total':total ,
+            'coupon': coupon,
+            'sub_total': sub_total,
+            'tax': tax,
+            'shipping': shipping,
+            'total': total,
             'discount': discount,
-            'grand': grand ,
+            'grand': grand,
             'receiver_name': request.POST.get('receiver_name'),
             'receiver_mobile': request.POST.get('receiver_mobile'),
-            'content': request.POST.get('content'),    
+            'content': request.POST.get('content'),
         })
-        order_serializer = OrderSerilizers(data=data)
-        if order_serializer.is_valid():
-            order = order_serializer.save()
-        else:
-            return Response({'errors':order_serializer.errors})
-        for item in cart.cart_items.all():
-            data= {
-                'product':item.product.pk,
-                'order':order.pk,
-                'warehouse_code':item.product.warehouse_code,
-                'price':item.product.get_after_discount_price(),
-                'quantity':item.quantity,
-            }
-
-            order_item = OrderItemSerilizers(data=data)
-            if order_item.is_valid():
-                order_item.save()
+        with transaction.atomic():
+            order_serializer = OrderSerilizers(data=data)
+            if order_serializer.is_valid():
+                order = order_serializer.save()
             else:
-                return Response({'errors':order_item.errors})
-        
-        return Response({'order_id':order.pk})
-  
+                return Response({'errors': order_serializer.errors})
+            order_helper = OrderHelper(order)
+            order_helper.add_order_items(cart)
+            if not order_helper.check_order()[0]:
+                return Response({'errors':  order_helper.check_order()[1]})
+                
+        return Response({'order_id': order.pk})
 
 
-  
 MERCHANT = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 ZP_API_REQUEST = "https://api.zarinpal.com/pg/v4/payment/request.json"
 ZP_API_VERIFY = "https://api.zarinpal.com/pg/v4/payment/verify.json"
@@ -114,35 +104,34 @@ description = "توضیحات مربوط به تراکنش را در این قس
 CallbackURL = 'http://127.0.0.1:8000/orders/verify/'
 
 
-
 class OrderPayView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, order_id):
         order = Order.objects.get(id=order_id)
         request.session['order_pay'] = {
-        	'order_id': order.id,
+            'order_id': order.id,
         }
         req_data = {
-        	"merchant_id": MERCHANT,
-        	"amount": float(order.grand),
-        	"callback_url": CallbackURL,
-        	"description": description,
-        	# "metadata": {"mobile": request.user.phone_number, "email": request.user.email}
+            "merchant_id": MERCHANT,
+            "amount": float(order.grand),
+            "callback_url": CallbackURL,
+            "description": description,
+            # "metadata": {"mobile": request.user.phone_number, "email": request.user.email}
         }
         req_header = {"accept": "application/json",
-        			  "content-type": "application/json'"}
+                      "content-type": "application/json'"}
         req = requests.post(url=ZP_API_REQUEST, data=json.dumps(
-        	req_data), headers=req_header)
+            req_data), headers=req_header)
         # TODO logging req.json()
-        
+
         if len(req.json()['errors']) == 0:
             authority = req.json()['data']['authority']
-            return redirect(ZP_API_STARTPAY.format(authority=authority))
+            return Response({'redirect': ZP_API_STARTPAY.format(authority=authority)})
         else:
             e_code = req.json()['errors']['code']
             e_message = req.json()['errors']['message']
-            return Response({"Error code":e_code, "Error Message": e_message})
+            return Response({"Error code": e_code, "Error Message": e_message})
 
 
 # class OrderVerifyView(LoginRequiredMixin, View):
