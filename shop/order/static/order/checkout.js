99ad9038c9264f1e81c4c8549a7b692c34a74a5f
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-05 08:38:11 +0330

    format doc

--- shop/order/static/order/checkout.js
+++ shop/order/static/order/checkout.js
@@ -1,36 +1,36 @@
-function checkoutHtml(data){
+function checkoutHtml(data) {
     $('#order-subtotal').text('$' + data.cart.grand_total)
-        $('#shipping').text('$' + data.shipping)
-        $('#tax').text('$' + data.tax)
-        $('#discount').text('$' + data.discount)
-        $('#total').text('$' + data.total)
+    $('#shipping').text('$' + data.shipping)
+    $('#tax').text('$' + data.tax)
+    $('#discount').text('$' + data.discount)
+    $('#total').text('$' + data.total)
 
-        let products = '';
-        for (const item of data.cart.cart_items){
-            products +='<tr class="mb-2">'
-            products +='<td class="product-name">'+ item.product.title +'<span'
-            products +='        class="product-quantity">×&nbsp;'+ item.quantity +'</span></td>'
-            products +='<td class="product-total text-body">  $'+ item.sub_total +'</td>'
-            products +='</tr>'
-        }
-        
-        $("#order-product").html('');
-        $("#order-product").html(products);
+    let products = '';
+    for (const item of data.cart.cart_items) {
+        products += '<tr class="mb-2">'
+        products += '<td class="product-name">' + item.product.title + '<span'
+        products += '        class="product-quantity">×&nbsp;' + item.quantity + '</span></td>'
+        products += '<td class="product-total text-body">  $' + item.sub_total + '</td>'
+        products += '</tr>'
+    }
+
+    $("#order-product").html('');
+    $("#order-product").html(products);
 }
 
 
-function couponMsg(msg){
-        let HtmlMsg = '';
-            HtmlMsg += '<div class="alert alert-primary alert-dark alert-round alert-inline">'
-			// HtmlMsg += '	<h4 class="alert-title">News :</h4>'
-			HtmlMsg += msg
-			HtmlMsg += '	<button type="button" class="btn btn-link btn-close">'
-			HtmlMsg += '		<i class="d-icon-times"></i>'
-			HtmlMsg += '	</button>'
-			HtmlMsg += '</div>'
-        
-        $("#coupon-msg").html('');
-        $("#coupon-msg").html(HtmlMsg);
+function couponMsg(msg) {
+    let HtmlMsg = '';
+    HtmlMsg += '<div class="alert alert-primary alert-dark alert-round alert-inline">'
+    // HtmlMsg += '	<h4 class="alert-title">News :</h4>'
+    HtmlMsg += msg
+    HtmlMsg += '	<button type="button" class="btn btn-link btn-close">'
+    HtmlMsg += '		<i class="d-icon-times"></i>'
+    HtmlMsg += '	</button>'
+    HtmlMsg += '</div>'
+
+    $("#coupon-msg").html('');
+    $("#coupon-msg").html(HtmlMsg);
 }
 
 $(document).ready(function () {
@@ -39,19 +39,19 @@ $(document).ready(function () {
         type: "GET",
         url: 'http://127.0.0.1:8000/api/v1/order/checkout/',
     }).done(function (data) {
-        checkoutHtml(data)  
+        checkoutHtml(data)
     });
 
 
     $("#applyCoupon").click(function (event) {
-        var coupon= $("#coupon").val();
-        url = 'http://127.0.0.1:8000/api/v1/order/checkout/?coupon='+coupon
+        var coupon = $("#coupon").val();
+        url = 'http://127.0.0.1:8000/api/v1/order/checkout/?coupon=' + coupon
         $.ajax({
             type: "GET",
             url: url,
         }).done(function (data) {
             checkoutHtml(data)
-            couponMsg(data.msg)  
+            couponMsg(data.msg)
         });
         event.preventDefault();
     });
@@ -68,7 +68,7 @@ $(document).ready(function () {
             receiver_mobile: $('#phone').val(),
             content: $('#order_notes').val(),
         };
-        
+
 
         var token = $("input[name=csrfmiddlewaretoken]").val();
         $.ajax({
@@ -80,19 +80,26 @@ $(document).ready(function () {
             encode: true,
         }).done(function (data) {
             console.log(data)
-            if (data.order_id){
+            if (data.order_id) {
                 $.ajax({
                     type: "GET",
-                    url: "http://127.0.0.1:8000/api/v1/order/order-pay/"+data.order_id,
+                    url: "http://127.0.0.1:8000/api/v1/order/order-pay/" + data.order_id,
                 }).done(function (data) {
+                    if (data.redirect) {
+                        window.location.replace(data.redirect);
+                    } else {
+                        console.log(111111111, data)
+                    }
 
-                    console.log(111111111, data)
-                });
+                }).fail(function () {
+                    alert("111111111111error");
+                });;
+            } else {
+                console.log(222222222, data)
             }
-            // window.location.replace("http://127.0.0.1:8000/api/v1/order/order-pay/"+data.order_id);
-        }).fail(function() {
-            alert( "error" );
-          });
+        }).fail(function () {
+            alert("error");
+        });
         event.preventDefault();
     });
 
