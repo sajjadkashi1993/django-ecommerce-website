93ae05e019a4caa88d56c1770153d101990f2066
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-06 12:54:30 +0330

    add 'api-token-auth url

--- shop/api/urls.py
+++ shop/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('product/', include("product.api.urls")),
     path('cart/', include("cart.api.urls")),
     path('order/', include("order.api.urls")),
     path('accounts/', include("accounts.api.urls")),
+    path('api-token-auth/', views.obtain_auth_token)
+
 ]
