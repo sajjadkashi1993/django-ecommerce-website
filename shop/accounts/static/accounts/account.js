2c4d5bd5637f5a09e94f2bf7cd08955a1512f6fe
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-08 12:21:38 +0330

    format doc

--- shop/accounts/static/accounts/account.js
+++ shop/accounts/static/accounts/account.js
@@ -1,50 +1,52 @@
-function msg(data){
+function msg(data) {
 }
 
 
-function customerOrder(data){
+function customerOrder(data) {
     $.ajax({
         type: "GET",
         url: 'http://127.0.0.1:8000/api/v1/order/customer-order-list/',
     }).done(function (data) {
-        let row=''
-        for (item of data){
-            row +='<tr>'
-            row +='    <td class="order-number"><a href="#">#'+item.id+'</a></td>'
-            row +='    <td class="order-date"><span>'+item.updated_at+'</span></td>'
-            row +='    <td class="order-status"><span>'+item.status+'</span></td>'
-            row +='    <td class="order-total"><span>$'+item.grand+'</span></td>'
-            row +='    <td class="order-action"><a href="#"'
-            row +='            class="btn btn-primary btn-link btn-underline">View</a></td>'
-            row +='</tr>'                               
+        console.log(data)
+        let row = ''
+        for (item of data) {
+            row += '<tr>'
+            row += '    <td class="order-number"><a href="#">#' + item.id + '</a></td>'
+            row += '    <td class="order-date"><span>' + item.updated_at + '</span></td>'
+            row += '    <td class="order-status"><span>' + item.status + '</span></td>'
+            row += '    <td class="order-total"><span>$' + item.grand + '</span></td>'
+            row += '    <td class="order-action"><a href="#"'
+            row += '            class="btn btn-primary btn-link btn-underline">View</a></td>'
+            row += '</tr>'
         }
         $('#orderList').html('')
         $('#orderList').html(row)
     });
 }
 
-function customerAddress(data){
+function customerAddress(data) {
     $.ajax({
         type: "GET",
         url: 'http://127.0.0.1:8000/api/v1/accounts/customer-adderss-list/',
     }).done(function (data) {
-        let row=''
-        for (item of data){
-        row +='<div class="col-sm-6 mb-4">'
-        row +='    <div class="card card-address">'
-        row +='        <div class="card-body">'
-        row +='            <h5 class="card-title text-uppercase">Address#'+item.id+'</h5>'
-        row +='            <p>'+item.country+'<br>'
-        row +='                '+item.province+'<br>'
-        row +='                '+item.city+'<br>'
-        row +='                '+item.address+'<br>'
-        row +='                '+item.postal_code
-        row +='            </p>'
-        row +='            <a href="#" class="btn btn-link btn-secondary btn-underline">Edit <i'
-        row +='                    class="far fa-edit"></i></a>'
-        row +='        </div>'
-        row +='    </div>'
-        row +='</div>           '
+        console.log(data)
+        let row = ''
+        for (item of data) {
+            row += '<div class="col-sm-6 mb-4">'
+            row += '    <div class="card card-address">'
+            row += '        <div class="card-body">'
+            row += '            <h5 class="card-title text-uppercase">Address#' + item.id + '</h5>'
+            row += '            <p>' + item.country + '<br>'
+            row += '                ' + item.province + '<br>'
+            row += '                ' + item.city + '<br>'
+            row += '                ' + item.address + '<br>'
+            row += '                ' + item.postal_code
+            row += '            </p>'
+            row += '            <a href="#" class="btn btn-link btn-secondary btn-underline">Edit <i'
+            row += '                    class="far fa-edit"></i></a>'
+            row += '        </div>'
+            row += '    </div>'
+            row += '</div>           '
         }
         $('#addresses').html('')
         $('#addresses').html(row)
@@ -61,7 +63,7 @@ $(document).ready(function () {
         console.log(url);
 
         var formData = {
-            first_name: $("#id_first_name").val(), 
+            first_name: $("#id_first_name").val(),
             last_name: $("#id_last_name").val(),
             email: $("#id_email").val(),
             gender: $("#id_gender").val(),
@@ -83,8 +85,8 @@ $(document).ready(function () {
             // console.log(status);
             msg(data)
             $("#msg").html('')
-            $("#msg").html('<div class="alert  alert-'+ data.status +' alert-dark alert-round alert-inline mb-1"><h4 class="alert-title">'+data.status+' :</h4>' +
-                data.msg+
+            $("#msg").html('<div class="alert  alert-' + data.status + ' alert-dark alert-round alert-inline mb-1"><h4 class="alert-title">' + data.status + ' :</h4>' +
+                data.msg +
                 '<button type="button" class="btn btn-link btn-close"><i class="d-icon-times"></i></button></div>');
         });
         event.preventDefault();
