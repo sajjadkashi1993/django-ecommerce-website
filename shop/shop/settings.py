d123a0e7606c1e650741ee922df6b376227b8d12
sajjadk <sajjad.kashi2012@gmail.com>
2023-01-08 12:20:04 +0330

    add throttling

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -185,5 +185,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/min',
+        'user': '1000/day'
+    },
     'PAGE_SIZE': 12
 }
